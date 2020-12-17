a5708a43c505a7c652df5bd18f25dd6d7ab7148a
Segundo <segundogaldames@gmail.com>
2020-12-16 22:53:34 -0300

    Administracion de roles

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::orderBy('nombre','ASC')->get(); #select * from roles order by nombre
+
+        return view('roles.index', compact('roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('roles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nombre' => 'required|string|min:4|unique:roles',
+        ]);
+
+        $role = new Role;
+        $role->nombre = $request->nombre;
+        $role->save();
+
+        return redirect('/roles')->with('success','El rol se ha registrado correctamente');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Role $role)
+    {
+        return view('roles.show', compact('role'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Role $role)
+    {
+        return view('roles.edit', compact('role'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Role $role)
+    {
+        $this->validate($request, [
+            'nombre' => 'required|string|min:4',
+        ]);
+
+        $role = Role::find($role->id); #select * from roles where id = role
+        $role->nombre = $request->nombre;
+        $role->save();
+
+        return redirect('/roles/' . $role->id)->with('success','El rol se ha modificado correctamente');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role)
+    {
+        //
+    }
+}
